a1316a48458ce3654ca9e62c3be9968e67e17595
Reinhard Korb <rkorb@korbconsulting.at>
2021-05-14 16:43:15 +0200

    added auth2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -8,20 +9,11 @@ Amplify.configure(awsExports);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
